b2e2d30fdc9ddac7799d2fd3701ab8c2958206bd
Abílio Nogueira <abilionbarros@gmail.com>
2024-01-09 22:24:25 -0300

    Adicionando os testes básicos.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Nokia cellNokia = new Nokia("88999989","Nokia C9","85954C8K2",128);
+            RealizarOperacoes(cellNokia,"Nokia");
+
+            Console.WriteLine();
+
+            Iphone cellIphone = new Iphone("88997788","Iphone 14","5855998CF",256);
+            RealizarOperacoes(cellIphone,"Iphone");
+        }
+
+         static void RealizarOperacoes(Smartphone smartphone, string tipoSmartphone)
+        {
+            Console.WriteLine($"{tipoSmartphone} - Número: {smartphone.Numero}, Modelo: {smartphone.Modelo}, IMEI: {smartphone.IMEI}, Memória: {smartphone.Memoria}GB");
+
+            smartphone.Ligar();
+            smartphone.ReceberLigacao();
+            smartphone.InstalarAplicativo("AppTeste");
+
+            Console.WriteLine();
+        }
+
+    }
+}
